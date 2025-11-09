66f9b1e336f623bcd1afa250918c91ab86038e78
alphaxt <pcwork45@gmail.com>
2025-11-09 16:48:00 +0500

    Fix URLs for PythonAnywhere deployment

--- djangoapp/djangoapp/urls.py
+++ djangoapp/djangoapp/urls.py
@@ -1,32 +1,19 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import TemplateView
 from djangoapp import views
 
-app_name = 'djangoapp'
 urlpatterns = [
-    # route is a string contains a URL pattern
-    # view refers to the view function
-    # name the URL
-
-    # path for about view
-    path(route='about', view=views.about, name='about'),
-
-    # path for contact us view
-    path(route='contact', view=views.contact, name='contact'),
-
-    # path for registration
-    path('registration/', views.registration_request, name='registration'),
-
-    # path for login
-    path('login/', views.login_request, name='login'),
-
-    # path for logout
-    path('logout/', views.logout_request, name='logout'),
-
-    # path for dealer reviews view
-    path(route='', view=views.get_dealerships, name='index'),
-
-    # path for add a review view
-    path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
-    path('dealer/<int:dealer_id>/add-review/', views.add_review, name='add_review'),
-
+    path('admin/', admin.site.urls),
+    path('djangoapp/', include([
+        path('about', views.about, name='about'),
+        path('contact', views.contact, name='contact'),
+        path('registration/', views.registration_request, name='registration'),
+        path('login/', views.login_request, name='login'),
+        path('logout/', views.logout_request, name='logout'),
+        path('', views.get_dealerships, name='index'),
+        path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
+        path('dealer/<int:dealer_id>/add-review/', views.add_review, name='add_review'),
+    ])),
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
