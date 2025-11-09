fb1f7c6e3e3eb678dce591bb23a6150bd785ce83
alphaxt <pcwork45@gmail.com>
2025-11-09 16:49:35 +0500

    Fix namespace registration

--- djangoapp/djangoapp/urls.py
+++ djangoapp/djangoapp/urls.py
@@ -3,17 +3,22 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 from djangoapp import views
 
+# App URLs with namespace
+app_name = 'djangoapp'
+app_urlpatterns = [
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
+    path('registration/', views.registration_request, name='registration'),
+    path('login/', views.login_request, name='login'),
+    path('logout/', views.logout_request, name='logout'),
+    path('', views.get_dealerships, name='index'),
+    path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
+    path('dealer/<int:dealer_id>/add-review/', views.add_review, name='add_review'),
+]
+
+# Main URL configuration
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('djangoapp/', include([
-        path('about', views.about, name='about'),
-        path('contact', views.contact, name='contact'),
-        path('registration/', views.registration_request, name='registration'),
-        path('login/', views.login_request, name='login'),
-        path('logout/', views.logout_request, name='logout'),
-        path('', views.get_dealerships, name='index'),
-        path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
-        path('dealer/<int:dealer_id>/add-review/', views.add_review, name='add_review'),
-    ])),
+    path('djangoapp/', include((app_urlpatterns, 'djangoapp'))),
     path('', TemplateView.as_view(template_name='index.html')),
 ]
